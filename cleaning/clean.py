6457a29e0325e990b90c05c3d50c9913a0f937ff
leaflettuce <andyjtrick@gmail.com>
2016-04-22 09:22:51 -0500

    reorganized, finished scrape and cleaning

---
+++ cleaning/clean.py
@@ -0,0 +1,58 @@
+import csv
+
+
+reader = csv.reader(open('spotify.csv', 'r'))
+header = reader.next()
+
+writer = csv.writer(open('spotify_clean.csv', 'wb'))
+header.remove('number')
+header.append('time')
+
+writer.writerow(header)
+
+dict = {}
+final = []
+
+for row in reader:
+
+    #dict = {tdwp : [band name, album name, popularity, release, tracks, genre], [etc]}
+    if row[0] in dict.keys():
+        dict[row[0]].append([row[0], row[1], row[2], row[3][:4], row[4], row[5]])
+
+    else:
+        dict[row[0]] = [[row[0], row[1], row[2], row[3][:4], row[4], row[5]]]
+
+
+for k,v in dict.iteritems():
+    checker = {}
+    number = -1
+
+    for each in v:
+        number += 1
+        release = int(each[3])
+        checker.update({release : number})
+
+    d = checker.keys()
+    first_date = min(d)
+    first = checker[first_date]
+    del checker[first_date]
+    e = v[first]
+
+    final.append([e[0], e[1], e[2], e[3], e[4], e[5], '0'])
+
+    while checker:
+
+        d = checker.keys()
+        second_date = min(d)
+        second= checker[second_date]
+        difference = second_date - first_date
+
+        del checker[second_date]
+        first_date = second_date
+
+        e = v[second]
+
+        final.append([e[0], e[1], e[2], e[3], e[4], e[5], difference])
+
+for album in final:
+    writer.writerow(album)
